4a7cb746e3a50403a3e2096cc29950d5d3656455
yorktronic <ty.walker@gmail.com>
2018-11-13 13:09:12 -0800

    corrected typo

--- zec_block_data.sh
+++ zec_block_data.sh
@@ -6,9 +6,9 @@ echo -n "[" >> zec_block_data.txt
 
 while [ $i -lt 409895 ]
 do
-        zcash-cli getblock $i >> zec_block_data.json
-        echo "," >> zec_block_data.json
+        zcash-cli getblock $i >> zec_block_data.txt
+        echo "," >> zec_block_data.txt
         i=$[$i+1]
 done
 
-echo -n "]" >> zec_block_data.json
+echo -n "]" >> zec_block_data.txt
